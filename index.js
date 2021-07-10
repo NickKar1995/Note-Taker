06788224f71b8eb07376e99683451a9db4d6f46a
NickTriesReallyHard <79810433+Xontrokolis@users.noreply.github.com>
2021-07-10 21:51:16 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,151 @@
+displayNotes();
+let addBtn = document.getElementById("addBtn");
+
+
+
+// //Enter button event listener//
+document.addEventListener("keydown", function(event) {
+  if (event.code === "Enter") {
+    keyPress(event);
+  }
+})
+// //
+//
+// //Enter Button function!//
+function keyPress(event) {
+
+
+}
+
+//
+
+
+
+addBtn.addEventListener("click", function() {
+  let notesObj;
+  let addNote = document.getElementById("addNote");
+  let notesString = localStorage.getItem("notes");
+
+
+  if (notesString === null) {
+    notesObj = [];
+
+  } else {
+    notesObj = JSON.parse(notesString)
+
+  }
+
+
+  //Add Date//
+
+
+  let now = new Date();
+  let dateTime = `${now.getDate()} - ${now.getMonth()+1} - ${now.getFullYear()} | ${now.getHours()}:${now.getMinutes()}`;
+
+  //Push Obj//
+  let tempObj = {
+    text: addNote.value,
+    time: dateTime
+  }
+  notesObj.push(tempObj);
+  localStorage.setItem("notes", JSON.stringify(notesObj))
+  addNote.value = "";
+  displayNotes();
+
+});
+
+
+
+
+
+
+//display data//
+
+function displayNotes() {
+  let notesObj;
+  let notesString = localStorage.getItem("notes");
+
+  if (notesString === null) {
+    notesObj = [];
+
+  } else {
+    notesObj = JSON.parse(notesString)
+  }
+
+  let html = "";
+  notesObj.forEach(function(element, index) {
+    html += `
+        <div class="card mx-4 my-2 bg-dark text-white thatsMyNote" style="width: 18rem;">
+          <div class="card-body">
+            <h6>${element.time}</h6>
+            <p class="card-text">${element.text.replace(/</g, "&lt;").replace(/>/g, "&gt;")}</p>
+            <button id="${index}" onclick=deleteNote(this.id) class="btn btn-danger">Delete</button>
+          </div>
+        </div>
+      `;
+
+  });
+
+  let noteEle = document.getElementById("notes");
+
+  if (notesObj.length != 0) {
+    noteEle.innerHTML = html;
+  } else {
+    noteEle.innerHTML = '<h3 style="text-align: center; color: grey;">Nothing to display</h3>';
+  }
+
+}
+
+
+function deleteNote(index) {
+  let notesObj;
+  let notesString = localStorage.getItem("notes")
+
+  if (notesString === null) {
+    notesObj = [];
+  } else {
+    notesObj = JSON.parse(notesString);
+  }
+
+
+  notesObj.splice(index, 1);
+  localStorage.setItem("notes", JSON.stringify(notesObj))
+
+  displayNotes();
+}
+
+let search = document.getElementById("search");
+search.addEventListener("input", function(e) {
+
+  let inputText = search.value
+
+  if (inputText == "") {
+    document.getElementById("noMatches").innerHTML = "";
+  }
+
+  var countNote = 0;
+
+  let cards = document.getElementByClassName("thatsMyNote")
+
+  Array.from(cards).forEach(function(ele) {
+
+    let cardText = ele.getElementsByTagName("p")[0].innerText;
+    if (cartText.includes(inputText)) {
+      ele.style.display = "block";
+
+    } else {
+      ele.style.dsiplay = "none";
+    }
+
+    countNone++;
+
+    if (countNone === cards.length) {
+      document.getElementById('noMatches').innerHTML = '<h3 style="text-align: center; color: grey;">No matches found</h3>';
+    } else {
+      document.getElementById('noMatches').innerHTML = '';
+
+    }
+
+  })
+
+});
